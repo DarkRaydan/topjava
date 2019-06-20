2f76e6bdef22d3c15cb4a65a30ee0b09d6b4cb40
DarkRaydan <Don.kartman2018@yandex.ru>
2019-06-20 18:33:37 +0300

    HW02 not complete:(

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
@@ -4,23 +4,30 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class InMemoryMealRepositoryImpl implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.MEALS.forEach(this::save);
+        MealsUtil.MEALS.forEach(x -> save(x,1));
+        save(new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "dinner1", 1000), 2);
+        save(new Meal(LocalDateTime.now().minusHours(2).truncatedTo(ChronoUnit.MINUTES), "dinner2", 1000), 2);
+        save(new Meal(LocalDateTime.now().minusHours(5).truncatedTo(ChronoUnit.MINUTES), "dinner3", 1), 2);
+        save(new Meal(LocalDateTime.now().minusDays(1).truncatedTo(ChronoUnit.MINUTES), "dinner3", 5500), 2);
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
+            meal.setUserId(userId);
             repository.put(meal.getId(), meal);
             return meal;
         }
